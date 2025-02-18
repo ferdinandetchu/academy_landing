c0a79550ab3087febc2d2660a7a28f54074bbaa5
ferdinandetchu <ferdinandetchu@gmail.com>
2025-02-18 14:30:46 +0100

    removed useState in component where it is not used

--- app/components/Curriculum/curriculum.tsx
+++ app/components/Curriculum/curriculum.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useState } from 'react'
 import DetailsCard from './details'
 import AiImg from './ai'
 
--- app/components/faqs/faqs.tsx
+++ app/components/faqs/faqs.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useState } from 'react'
 import FaqsCard from './details'
 
 const faqs = [
